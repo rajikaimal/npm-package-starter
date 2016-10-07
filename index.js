5468170be680d900cee3e7552eb706f8dd278b1e
rajikaimal <inbox.rajika@gmail.com>
2016-10-07 20:14:45 +0530

    nps

---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env/node
+
+const chalk = require('chalk');
+const ora = require('ora');
+const shelljs = require('shelljs/global');
+const execSync = require('child_process').execSync;
+
+if(process.argv[2] === undefined) {
+	console.log(chalk.red('provide a package name'));
+	return;
+}
+
+const spinner = ora(`creating package ${process.argv[2]}`).start();
+
+mkdir('-p', process.argv[2]);
+//copy files with names
+cp('templates/*', process.argv[2]);
+//copy dot-files
+cp('-R', 'templates/.*', process.argv[2]);
+
+if(process.argv[3] === '-m') {
+	spinner.text = 'creating mocha tests';
+	cp('-R', 'templates/mocha/test', process.argv[2]);
+}
+else if(process.argv[3] === '-a') {
+	spinner.text = 'creating ava tests';
+	cp('-R', 'templates/ava/test', process.argv[2]);
+}
+else {
+	spinner.text = 'please provide correct options';
+	spinner.fail();
+	console.log(`
+	Usage:
+	$ nps -m - package with mocha tests
+	$ nps -a - package with ava tests
+	`);
+	return;
+}
+
+cd(process.argv[2]);
+execSync('npm init', {stdio: 'inherit'});
+
+spinner.text = 'package structure created !';
+spinner.succeed();
+
+console.log(`
+	$ cd ${process.argv[2]}
+	$ npm install
+`);
+
+console.log(`Happy hacking`);
